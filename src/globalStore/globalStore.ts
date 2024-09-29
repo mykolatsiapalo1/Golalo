f29097e6ef8bd8ea393563c1309e5300bca97d43
Mykola Tsiapalo <mykolatsiapalo1@gmail.com>
2024-09-29 04:09:37 +0300

    clean up 2

--- src/globalStore/globalStore.ts
+++ src/globalStore/globalStore.ts
@@ -1,13 +1,19 @@
 import { create } from "zustand";
 
+enum UserRole {
+  Household = "Household",
+  Tradie = "Tradie",
+  Admin = "Admin",
+}
+
 export interface IUser {
-  id: string;
+  id: number;
   name: string;
   lastName: string;
   tag: string;
   email: string;
   avatar: string;
-  role: "Household" | "Tradie" | "Admin";
+  role: UserRole;
   notifications: {
     id: number;
     title: string;
@@ -16,10 +22,12 @@ export interface IUser {
     read: boolean;
   }[];
   categoriesHired: {
+    id: number;
     title: string;
   }[];
   location: string;
   reviews: {
+    id: number;
     rating: number;
   }[];
   type: string;
@@ -35,23 +43,13 @@ export interface IUser {
     startDate: string;
     endDate: string;
     status: string;
-    hiredTradie: {
-      name: string;
-      avatar: string;
-      tag: string;
-    };
+    hiredTradie: Pick<IUser, "name" | "avatar" | "tag">;
     location: string;
     completed: boolean;
-    nextTask: {
-      title: string;
-      description: string;
-      status: string;
-      date: string;
-      category: string;
-      color: string;
-    };
+    nextTask: IUser["tasks"][number];
   }[];
   tasks: {
+    id: number;
     title: string;
     description: string;
     status: string;
@@ -60,16 +58,18 @@ export interface IUser {
     color: string;
   }[];
   messages: {
+    id: number;
     title: string;
     date: string;
   }[];
 }
 
-const MOCK_TASKS = ["#FFFFFF", "#FF7125", "#DC3545"].map((color, index) => ({
-  title: `Task ${index + 1}`,
+const MOCK_TASKS = ["#FFFFFF", "#FF7125", "#DC3545"].map((color, id) => ({
+  id,
+  title: `Task ${id + 1}`,
   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sem leo, suscipit eget elit porttitor, volutpat consequat turpis. In interdum mauris et urna maximus, sed tincidunt nulla porta. Maecenas congue risus enim. Quisque egestas augue sem, nec egestas leo mattis ut. Phasellus non sem eu massa finibus volutpat. Vestibulum condimentum augue odio, in dapibus erat vehicula ut. Praesent ac massa at urna dignissim iaculis egestas ut felis. Nullam pharetra, lorem non congue fringilla, nisl ex ullamcorper nibh, nec vehicula nulla libero id dui. Pellentesque bibendum pulvinar tempor. Fusce sodales tincidunt ex, non aliquet arcu consectetur posuere. Mauris suscipit auctor tempor. Phasellus a finibus felis, vel porta nibh. Ut laoreet purus sed enim feugiat, non bibendum nunc semper. Cras aliquam velit in tincidunt pulvinar. Morbi vitae dictum nibh. Sed viverra nunc sit amet libero tincidunt euismod.`,
   status: "open",
-  date: new Date(Date.now() + index * 2 * 24 * 60 * 60 * 1000).toISOString(),
+  date: new Date(Date.now() + id * 2 * 24 * 60 * 60 * 1000).toISOString(),
   category: "Accounting",
   color,
 }));
@@ -103,20 +103,25 @@ const MOCK_LISTINGS: IUser["listings"] = Array.from(
   })
 );
 
-const MOCK_REVIEWS: IUser["reviews"] = Array.from({ length: 10 }, (_) => ({
+const MOCK_REVIEWS: IUser["reviews"] = Array.from({ length: 10 }, (_, id) => ({
+  id,
   rating: 5,
 }));
 
-const MOCK_MESSAGES: IUser["messages"] = Array.from({ length: 10 }, (_) => ({
-  title: "New Message",
-  date: new Date(Date.now() - 2 * 60 * 60 * 1000).toISOString(),
-}));
+const MOCK_MESSAGES: IUser["messages"] = Array.from(
+  { length: 10 },
+  (_, id) => ({
+    id,
+    title: "New Message",
+    date: new Date(Date.now() - 2 * 60 * 60 * 1000).toISOString(),
+  })
+);
 
 const MOCK_CATEGORIES: IUser["categoriesHired"] = [
   "Accounting",
   "Architect",
   "Bricklayer",
-].map((title) => ({ title }));
+].map((title, id) => ({ id, title }));
 
 const MOCK_NOTIFICATIONS: IUser["notifications"] = Array.from({
   length: 3,
@@ -130,14 +135,14 @@ const MOCK_NOTIFICATIONS: IUser["notifications"] = Array.from({
 }));
 
 const MOCK_USER: IUser = {
-  id: "1",
+  id: 0,
   name: "John",
   lastName: "Doe",
   email: "john.doe@example.com",
   avatar:
     "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
   tag: "johndoe",
-  role: "Household",
+  role: UserRole.Household,
   notifications: MOCK_NOTIFICATIONS,
   categoriesHired: MOCK_CATEGORIES,
   location: "Sydney",
